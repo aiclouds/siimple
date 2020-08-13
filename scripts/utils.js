42ee416b0de67ba738893d9d83a913dc1d018dce
Josemi <josemijuanes@gmail.com>
2020-08-14 00:53:06 +0200

    Removed unused script for creating symlinks to modules and updated utils

--- scripts/utils.js
+++ scripts/utils.js
@@ -24,5 +24,24 @@ module.exports.getPackages = function (parent) {
     return pkgs;
 };
 
+//Read a JSON file
+module.exports.readJSON = function (file) {
+    return JSON.parse(fs.readFileSync(file, "utf8"));
+};
 
+//Write a JSON file
+module.exports.writeJSON = function (file, content) {
+    return fs.writeFileSync(file, JSON.stringify(content), "utf8");
+};
+
+//For displaying basic logs
+module.exports.log = {
+    "warn": function (message) {
+        return console.warn(`[WARNING] ${message}`);
+    },
+    "error": function (message) {
+        console.error(`[ERROR] ${message}`);
+        return process.exit(1); //Stop process
+    }
+};
 
