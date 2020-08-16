b94b616e42346e4496f1606e53f339e716b76f61
Josemi <josemijuanes@gmail.com>
2020-08-16 12:49:34 +0200

    Added components entryfiles for react components and styles

---
+++ src/components/index.js
@@ -0,0 +1,74 @@
+//Export utils
+export {classNames} from "../utils/classnames.js";
+export {createStorage, Storage} from "../utils/storage.js";
+export {whenReady} from "../utils/ready.js";
+export {global, createGlobalStorage} from "../utils/global.js";
+
+//Elemental components
+export * from "./elements/Alert.js";
+export * from "./elements/Breadcrumb.js";
+export * from "./elements/Btn.js";
+export * from "./elements/Card.js";
+export * from "./elements/List.js";
+export * from "./elements/Menu.js";
+export * from "./elements/Rule.js";
+export * from "./elements/Spinner.js";
+export * from "./elements/Table.js";
+export * from "./elements/Tabs.js";
+export * from "./elements/Tag.js";
+export * from "./elements/Tip.js";
+
+//Typography components
+export * from "./typography/Typography.js";
+
+//Experimental components
+export * from "./experiments/Media.js";
+export * from "./experiments/Modal.js";
+export * from "./experiments/Progress.js";
+
+//Form components
+export * from "./form/Checkbox.js";
+export * from "./form/Field.js";
+export * from "./form/Input.js";
+export * from "./form/Label.js";
+export * from "./form/Radio.js";
+export * from "./form/Select.js";
+export * from "./form/Switch.js";
+export * from "./form/Textarea.js";
+
+//Grid components
+export * from "./grid/Column.js";
+export * from "./grid/Row.js";
+
+//Icons
+export {Icon} from "./icon/Icon.js";
+
+//Layout components
+export * from "./layout/Content.js";
+export * from "./layout/Footer.js";
+export * from "./layout/Jumbotron.js";
+export * from "./layout/Navbar.js";
+
+//Utils components
+export * from "./utils/Choose.js";
+export * from "./utils/For.js";
+export * from "./utils/ForEach.js";
+export * from "./utils/If.js";
+export * from "./utils/Renderer.js";
+
+//Future components
+export * from "./future/Appbar/index.js";
+//export * from "./future/BottomSide/index.js";
+//export * from "./future/Centered/index.js";
+//export * from "./future/Check/index.js";
+export * from "./future/Drawer/index.js";
+export * from "./future/Group/index.js";
+export * from "./future/Item/index.js";
+export * from "./future/Panel/index.js";
+export * from "./future/Placeholder/index.js";
+export * from "./future/Range/index.js";
+export * from "./future/Scrim/index.js";
+export * from "./future/SplitPanel/index.js";
+export * from "./future/Stepper/index.js";
+export * from "./future/Toolbar/index.js";
+
