f813cfc89039c52e7c639dd372586194da29c10e
Josemi <josemijuanes@gmail.com>
2020-08-16 13:58:34 +0200

    Init global entryfile

---
+++ src/index.js
@@ -0,0 +1,15 @@
+//Export components
+export * from "./components/index.js";
+
+//Export other elements and components
+export * from "./datatable/index.js";
+export * from "./editor/index.js";
+export * from "./widgets/index.js";
+
+//Export utils
+//export {classNames} from "./utils/classnames.js";
+//export {createStorage, Storage} from "./utils/storage.js";
+//export {whenReady} from "./utils/ready.js";
+//export {global, createGlobalStorage} from "./utils/global.js";
+
+
