8a01c95713fc395669cf3a965af88e726e52cdcf
Josemi <josemijuanes@gmail.com>
2020-08-16 13:57:11 +0200

    Added utils entryfile

---
+++ src/utils/index.js
@@ -0,0 +1,5 @@
+export {classNames} from "./classnames.js";
+export {global, createGlobalStorage} from "./global.js";
+export {whenReady} from "./ready.js";
+export {createStorage} from "./storage.js";
+
