277a7b23a25d071179e8b3d2f9c94c8741d8cc6a
Josemi <josemijuanes@gmail.com>
2020-08-16 12:50:13 +0200

    Init global constants

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+//Export keycodes
+export const keyCodes = {
+    "backspace": 8,
+    "tab": 9,
+    "enter": 13,
+    "escape": 27,
+    "space": 32,
+    "arrowLeft": 37,
+    "arrowUp": 38,
+    "arrowRight": 39,
+    "arrowDown": 40,
+    "delete": 46
+}
+
