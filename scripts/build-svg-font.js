6f1b1d8ba88373adc482aad7209a72609e987b6c
Josemi <josemijuanes@gmail.com>
2020-08-18 15:09:57 +0200

    Moved all scripts for building icons fonts and for compiling icons list to scripts root folder

--- scripts/build-svg-font.js
+++ scripts/build-svg-font.js
@@ -2,36 +2,25 @@ let fs = require("fs");
 let path = require("path");
 let SVGIcons2SVGFontStream = require("svgicons2svgfont");
 let getArgs = require("get-args");
-
-//Import icons
-let icons = require("../icons.json");
-
-//Function that does nothing
-let noop = function () {
-    return;
-};
-
-//Print the generated error and exit
-let exitScript = function (error) {
-    process.stderr.write(error.message);
-    return process.exit(1);
-}
+let paths = require("../config/paths.js");
+let utils = require("./utils.js");
 
 //Build the SVG font
 process.nextTick(function () {
+    let icons = utils.readJSON(paths.icons);
     let options = getArgs().options;
-    let fontOptions = {
+    let fontStream = new SVGIcons2SVGFontStream({
         "fontName": "SiimpleIcons", 
         "normalize": true, 
         "fontHeight": 1000, 
-        "log": noop
-    }; 
-    let fontStream = new SVGIcons2SVGFontStream(fontOptions);
+        "log": function () { return; }
+    });
     let writer = fs.createWriteStream(options.output);
     fontStream.pipe(writer);
     //Error listener
     fontStream.on('error', function (error) {
-        return exitScript(error);
+        process.stderr.write(error.message);
+        return process.exit(1);
     });
     //Writer finished
     writer.on('finish', function () {
@@ -40,10 +29,14 @@ process.nextTick(function () {
     //Add each icon in the font stream
     icons.forEach(function (icon) {
         //process.stdout.write("Adding icon '" + icon.id + "' to SVG font");
-        let iconPath = path.join("./svg/", icon.id + ".svg");
+        let iconPath = path.join(paths.src.icons, `${icon.id}.svg`);
         let iconReader = fs.createReadStream(iconPath);
         //Set the icon metadata
-        iconReader.metadata = {"unicode": [String.fromCharCode(icon.unicode)], "name": icon.id};
+        iconReader.metadata = {
+            "unicode": [String.fromCharCode(icon.unicode)], 
+            "name": icon.id
+        };
+        //Write the icon
         fontStream.write(iconReader);
     });
     fontStream.end();
