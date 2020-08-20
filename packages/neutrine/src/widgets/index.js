a33f4b48ded892be91d7ea9efdbabbfacaae8e4c
Josemi <josemijuanes@gmail.com>
2020-08-20 20:10:48 +0200

    Changed structure of widgets module and fixed some imports in neutrine package

--- packages/neutrine/src/widgets/index.js
+++ packages/neutrine/src/widgets/index.js
@@ -1,6 +1,25 @@
-//export * from "./cookies-consent/index.js";
-//export * from "./text-editor/index.js";
-export * from "./toast/index.js";
-export * from "./confirm/index.js";
-export * from "./loading/index.js";
+import React from "react";
+import ReactDOM from "react-dom";
+
+import {Confirmer} from "./Confirm/index.js";
+import {Loader} from "./Loading/index.js";
+import {Toast} from "./Toast/index.js";
+
+//Factory method
+let factoryComponent = function (component) {
+    return function (props, parent) {
+        //Check for no parent element provided
+        if (typeof parent !== "object" || parent === null) {
+            parent = document.createElement("div");
+            document.body.appendChild(parent);
+        }
+        //Mount the component
+        return ReactDOM.render(React.createElement(component, props), parent);
+    };
+};
+
+//Export confirmers
+export const createConfirmer = factoryComponent(Confirmer);
+export const createLoader = factoryComponent(Loader);
+export const createToaster = factoryComponent(Toast);
 
