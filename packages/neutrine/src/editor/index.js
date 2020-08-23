72075731d053059126b5ad4b661c983b4753b169
Josemi <josemijuanes@gmail.com>
2020-08-23 19:55:16 +0200

    Added entryfile for neutrine editor module

---
+++ packages/neutrine/src/editor/index.js
@@ -0,0 +1,8 @@
+//Export components
+export {EditorComponent} from "./components/editor.js";
+
+//Export editor methods
+export {colorize} from "./colorize.js";
+export {createEditor, defaultOptions as defaultEditorOptions} from "./editor.js";
+export {createSandbox, defaultOptions as defaultSandboxOptions} from "./sandbox.js";
+
