be861aba4a61a3de6c2b9bd255d0dc5234b88753
Josemi <josemijuanes@gmail.com>
2020-09-13 13:10:34 +0200

    Added Navside component to neutrine future components

---
+++ packages/neutrine/src/components/future/Navside/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import {htmlElement} from "../../../utils/reactElements.js";
+import {classNames} from "../../../utils/classnames.js";
+
+import "./style.scss";
+
+//Export navside component
+export const Navside = htmlElement("div", "siimple__navside");
+
